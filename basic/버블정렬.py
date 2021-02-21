c859ddddaedb0ef627ab5d3245008ff72199c61d
Yeonji Lee <yeonji.dev@gmail.com>
2021-02-21 23:05:06 +0900

    버블정렬 수정

--- basic/버블정렬.py
+++ basic/버블정렬.py
@@ -1,9 +1,6 @@
-# arr = [] 리스트 입력
-
-def BubbleSort(a) :
-    for i in range(len(a)-1, 0, -1) :
-        for j in range(0, i) :
-            if a[j] > a[j+1] :
+def bubble_sort1(a):
+    for i in range(len(a)-1, 0, -1):
+        for j in range(0, i):
+            if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
-BubbleSort(arr)
-print(arr)
+    return a
