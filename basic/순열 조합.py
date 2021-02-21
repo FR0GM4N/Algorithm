8999b974c94e09aa8ad99e567b9007c8cd370b21
Yeonji Lee <yeonji.dev@gmail.com>
2021-02-21 23:04:25 +0900

    순열조합 출력결과 주석추가

--- basic/순열 조합.py
+++ basic/순열 조합.py
@@ -3,9 +3,9 @@ import itertools
 # 순열
 arr = ['A', 'B', 'C']
 nPr = itertools.permutations(arr, 2)
-print(list(nPr))
+print(list(nPr))  #=> [('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'C'), ('C', 'A'), ('C', 'B')]
 
 # 조합
 arr = ['A', 'B', 'C']
 nCr = itertools.combinations(arr, 2)
-print(list(nCr))
+print(list(nCr))  #=> [('A', 'B'), ('A', 'C'), ('B', 'C')]
