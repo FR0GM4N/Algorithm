1faacc41ac95feb0d2317978c5dca8bb71c14ad0
Yeonji Lee <yeonji.dev@gmail.com>
2021-02-23 15:14:58 +0900

    4869 종이붙이기 추가

---
+++ swea/4869_[파이썬 SW 문제해결 기본] 4일차 - 종이붙이기/sol1.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open("sample_input.txt")
+
+# 재귀함수
+def paper_cut(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 3
+    return paper_cut(n-1) + paper_cut(n-2) * 2
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    ans = paper_cut(N//10)
+
+    print("#{} {}".format(tc, ans))
