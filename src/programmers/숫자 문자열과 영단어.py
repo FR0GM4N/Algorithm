8819b5d5815d7bd4d6294ee60314b07e7ed1868d
Yeonji Lee <yeonji.dev@gmail.com>
2021-08-11 13:11:32 +0900

    upgrade code

--- src/programmers/숫자 문자열과 영단어.py
+++ src/programmers/숫자 문자열과 영단어.py
@@ -1,13 +1,8 @@
 def solution(s):
-    answer = ''
-    dict = {'ze':['0',4], 'on':['1',3], 'tw':['2',3], 'th':['3',5], 'fo':['4',4], 'fi':['5',4], 'si':['6',3], 'se':['7',5], 'ei':['8',5], 'ni':['9',4],} # [value, len]
-    i = 0
-    nums = ['0','1','2','3','4','5','6','7','8','9']
-    while i < len(s):
-        if s[i] in nums:
-            answer += s[i]
-            i += 1
-        else:
-            answer += dict[s[i:i+2]][0]
-            i += dict[s[i:i+2]][1]
-    return int(answer)
+    alpha = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
+             'nine': 9, }
+
+    for key, val in alpha.items():
+        s = s.replace(key, str(val))
+
+    return int(s)
